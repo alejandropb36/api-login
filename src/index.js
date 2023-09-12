1cf67b89bfdfc3ac0bbf2366f4251738455b4461
Alejandro Ponce <alexponce36@hotmail.com>
2023-09-12 13:58:02 -0600

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-import 'dotenv/config'; // env
-import app from './app.js';
-import { sequelize } from './config/database.js';
+import "dotenv/config"; // env
+import app from "./app.js";
+import { sequelize } from "./config/database.js";
+import cors from "cors";
 
-import './models/User.js';
+import "./models/User.js";
 
 async function main() {
+  try {
+    // CORS
+    const whitelist = [
+      "http://127.0.0.1:4200",
+      "http://127.0.0.1:3000",
+      "http://localhost:4200",
+      "http://localhost:3000",
+      "http://login.alejandropb.net",
+      "https://login.alejandropb.net",
+      "https://*.alejandropb.net",
+    ];
 
-    try {
-        const PORT = process.env.PORT || 4000;
-        
-        await sequelize.sync();
+    const corsOptions = {
+      origin: function (origin, callback) {
+        if (whitelist.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Error de cors"));
+        }
+      },
+    };
 
-        app.listen(PORT, () => {
-            console.log(`Server is listening on port: ${PORT}`);
-        });
+    app.use(cors(corsOptions));
 
-    } catch(error) {
-        console.error('Error to run app:', error);
-    }
+    const PORT = process.env.PORT || 4000;
+
+    await sequelize.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error to run app:", error);
+  }
 }
 
 main();
