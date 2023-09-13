8348a6cdbd72491625a4d13589bbb82d12b01214
Alejandro Ponce <alexponce36@hotmail.com>
2023-09-12 23:01:47 -0600

    fix: cors again

--- src/app.js
+++ src/app.js
@@ -5,32 +5,7 @@ import cors from "cors";
 
 const app = express();
 // CORS
-const whitelist = [
-  "http://127.0.0.1",
-  "http://localhost",
-  "http://127.0.0.1:4200",
-  "http://127.0.0.1:3000",
-  "http://localhost:4200",
-  "http://localhost:3000",
-  "http://login.alejandropb.net",
-  "https://login.alejandropb.net",
-  "http://api-login.alejandropb.net",
-  "https://api-login.alejandropb.net",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log(origin);
-    if (whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Error de cors"));
-    }
-  },
-  credentials: true
-};
-
-app.use(cors(corsOptions));
+app.use(cors({credentials: true}));
 
 // Middlewares
 app.use(express.json());
